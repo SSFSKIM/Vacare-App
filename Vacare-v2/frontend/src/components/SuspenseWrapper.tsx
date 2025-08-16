08a6c7284a1706c4122e3966ace1aff53ff29cf6
SSFSKIM <kimmi@ssfs.org>
2025-08-16 20:01:11 +0900

    wrong commit

--- Vacare-v2/frontend/src/components/SuspenseWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Suspense, ReactNode } from 'react';
-
-interface SuspenseWrapperProps {
-  children: ReactNode;
-}
-
-export function SuspenseWrapper({ children }: SuspenseWrapperProps) {
-  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
-}
